4b0d4e6fb71bd9d232ab1151e78549db54fbad54
mujawar aqdas ahmad adam <aqdasmujawar148@gmail.com>
2026-08-06 22:38:42 +0530

    Fix typo in BoatsPage.jsx finally block

--- client/src/pages/BoatsPage.jsx
+++ client/src/pages/BoatsPage.jsx
@@ -25,7 +25,7 @@ export default function BoatsPage() {
       setBoats(res);
     } catch (err) {
       console.error(err);
-    } fontally {
+    } finally {
       setLoading(false);
     }
   };
